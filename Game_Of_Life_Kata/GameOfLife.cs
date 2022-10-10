d8b438779cfac1bfe655bb40575f28a06d5c7a1c
Dave Bage <david_bage@hotmail.com>
2022-10-10 15:41:17 +0100

    refactor tests Move two dimensional cell array into Universe class

--- Game_Of_Life_Kata/GameOfLife.cs
+++ Game_Of_Life_Kata/GameOfLife.cs
@@ -2,45 +2,20 @@
 {
     public class GameOfLife
     {
-        private bool[,] _cells;
-        public bool Seed(bool[] seedPattern)
+        private Universe _universe;
+
+        public bool Seed(bool[,] seedPattern)
         {
             if (seedPattern.Length == 0) throw new ArgumentException();
 
-            _cells = new bool[1, seedPattern.Length];
-
-            for (int columnIndex = 0; columnIndex < seedPattern.Length; columnIndex++)
-                _cells[0, columnIndex] = seedPattern[columnIndex];
+            _universe = new Universe(seedPattern);
  
-            return true;
-        }
-        public bool Seed(bool[,] seedPattern)
-        {
-            if (seedPattern == null) return false;
-
-            _cells = seedPattern;
             return true;
         }
 
         public bool[,] Tick()
         {
-            var result = new bool[_cells.GetLength(0), _cells.GetLength(1)];
-
-            for (int columnIndex = 0; columnIndex <= _cells.GetLength(0) - 1; columnIndex++)
-            {
-                var liveNeighbours = 0;
-
-                if (columnIndex - 1 >= 0 && _cells[0, columnIndex - 1])
-                    liveNeighbours++;
-
-                if (columnIndex + 1 <= _cells.GetLength(0) - 1 && _cells[0, columnIndex + 1])
-                    liveNeighbours++;
-
-                result[0, columnIndex] = false;
-
-                if (liveNeighbours == 2)
-                    result[0, columnIndex] = true;
-            }
+            var result = _universe.NextGeneration();
 
             return result;
 
---
+++ Game_Of_Life_Kata/Universe.cs
@@ -0,0 +1,34 @@
+﻿namespace Game_Of_Life_Kata;
+
+public class Universe
+{
+    private readonly bool[,] _cells;
+
+    public Universe(bool[,] cells)
+    {
+        _cells = cells ?? throw new ArgumentNullException(nameof(cells));
+    }
+
+    public bool[,] NextGeneration()
+    {
+        var result = new bool[_cells.GetLength(0), _cells.GetLength(1)];
+
+        for (int columnIndex = 0; columnIndex <= _cells.GetLength(0) - 1; columnIndex++)
+        {
+            var liveNeighbours = 0;
+
+            if (columnIndex - 1 >= 0 && _cells[0, columnIndex - 1])
+                liveNeighbours++;
+
+            if (columnIndex + 1 <= _cells.GetLength(0) - 1 && _cells[0, columnIndex + 1])
+                liveNeighbours++;
+
+            result[0, columnIndex] = false;
+
+            if (liveNeighbours == 2)
+                result[0, columnIndex] = true;
+        }
+
+        return result;
+    }
+}
